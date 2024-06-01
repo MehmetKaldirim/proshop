b857c047aa57620f16b4f327f1fa3f40c171361a
Mehmet <mathdenizi@gmail.com>
2024-06-01 03:21:13 +0200

    asyncHandler and errorMiddleware works  controller have a bug

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+import { getProductById, getProducts } from "../controllers/productController";
+
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
+
+export default router;
