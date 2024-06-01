e5b76d929d2a84216f62461b5d63d2275159e1f4
Mehmet <mathdenizi@gmail.com>
2024-06-01 06:14:16 +0200

    Error and Loading spinner added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,19 +1,22 @@
 import { Row, Col } from "react-bootstrap";
 
-//import products from "../products";
 import Product from "../components/Product";
 
 import React from "react";
 import { useGetProductsQuery } from "../slices/productSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
   return (
     <>
       {isLoading ? (
-        <h2>Loading</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <h1>Latest Products</h1>
