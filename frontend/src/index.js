4cf820a4594141d6d9dab816a70451bc65c91310
Mehmet <mathdenizi@gmail.com>
2024-06-03 00:33:42 +0200

    shipping screen added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CardScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
